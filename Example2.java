3f97df2fe12adb2b6cd4ac9d4a20cc1a1fc54126
77ehtesham-alam <ehtesham.alam@apphelix.ai>
2025-05-08 15:02:47 +0530

    Update Example2.java

--- Example2.java
+++ Example2.java
@@ -1,1 +1,13 @@
+public class Example2 {
+    public static void main(String[] args) {
+        System.out.println("Hello, this is Example2!");
+        // Call the method to calculate the area of a rectangle
+        double area = calculateArea(5.0, 3.0);
+        System.out.println("The area of the rectangle is: " + area);
+    }
+
+    public static double calculateArea(double length, double width) {
+        return length * width;
+    }
+}
 
