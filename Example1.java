a2ed5f1065a39d8b27bae5522752a2b809f87ae6
77ehtesham-alam <ehtesham.alam@apphelix.ai>
2025-05-08 15:00:08 +0530

    Create Example1.java

---
+++ Example1.java
@@ -0,0 +1,12 @@
+public class Example1 {
+    public static void main(String[] args) {
+        System.out.println("Hello, this is Example1!");
+        // Add some basic logic
+        int result = addNumbers(5, 3);
+        System.out.println("The sum is: " + result);
+    }
+
+    public static int addNumbers(int a, int b) {
+        return a + b;
+    }
+}
